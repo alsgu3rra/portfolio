958f0f0d9bfae175f89360b5ee518badc7593568
Alan Lima da Silva Guerra <164754267+als-guerra@users.noreply.github.com>
2024-06-11 12:34:16 -0300

    Alterando imagem e conteudo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,18 +17,16 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Olá, Eu sou{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Alan Guerra",
                 1000,
-                "Web Developer",
+                "Dev Web",
                 1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                "Dev Mobile",
                 1000,
               ]}
               wrapper="span"
@@ -37,22 +35,21 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          Programador de Sistemas Back-End com 7 anos de Experiência com desenvolvimento web, 4 anos de experiência com projetos freelancers, Criador de Conteúdo, Professor de TI, Estudante de DevOps e Microsserviços
           </p>
           <div>
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Sobre Mim
             </Link>
             <Link
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Baixar CV
               </span>
             </Link>
           </div>
@@ -65,11 +62,11 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero.avif"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={350}
+              height={350}
             />
           </div>
         </motion.div>
